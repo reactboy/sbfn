cc8d2ca9edb5ce63d296e3f4aaf439670bb00a1b
reactboy <junceliz@gmail.com>
2021-02-06 20:26:53 +0900

    add: add strapi packages for cms

---
+++ packages/sbfn-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '04e53b1390916a38cefd70472780e0fb'),
+    },
+  },
+});
---
+++ packages/sbfn-strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2a7d5c1b-c550-4a67-9f17-44e58c8c5ac7'
+};
